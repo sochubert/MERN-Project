ac4bba15d76091003f43c2c277b75749cbc34807
sochubert <sochubert@gmail.com>
2022-05-01 14:42:09 +0900

    Nav & Link

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ import {
 } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
 import Users from "./user/pages/Users";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-        {/* 없는 경로가 나오면 /로 redirection */}
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+          {/* 없는 경로가 나오면 /로 redirection */}
+        </Switch>
+      </main>
     </Router>
   );
 };
